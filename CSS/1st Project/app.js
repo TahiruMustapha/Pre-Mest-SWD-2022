fb4b24868d7535aba76784558664b0439dddc5ad
TahiruMustapha <79904219+TahiruMustapha@users.noreply.github.com>
2022-08-05 06:44:35 -0700

    Added CSS file

---
+++ CSS/1st Project/app.js
@@ -0,0 +1,19 @@
+
+// for(let i = 1; i <= 20; i++){
+//     console.log(i);
+
+
+// let numbers = [1,2,3,4,5];
+// let newArra = [];
+// for(let i = 0; i<numbers.length; i++){
+//     newArra.push(numbers[i]);
+//     console.log(newArra)
+// }
+ 
+// console.log(numbers.length)
+let countries = ["Ghana", "Nigeria","Togo","Mali", "Tonga"]
+let empty = []
+for(let i = 0; i<countries.length; i++){
+    empty.push(countries[i].toUpperCase())
+console.log(empty);
+}
